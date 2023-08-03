2577539ace729b0724dbe7cff52d5bf172502d9b
josiasOLG <48293404+josiasOLG@users.noreply.github.com>
2023-08-03 18:21:32 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,39 +14,39 @@ app.use(cors({
 let jobs = [
 	{ 
 		"ID": 1,
-		"Descrição": "Importação de arquivos de fundos", 
-		"Data Máxima de conclusão": '2021-02-04 12:00:00', 
-		"Tempo estimado": '8 horas'
+		"Descricao": "Importação de arquivos de fundos", 
+		"Data_Maxima_de_conclusao": '2021-02-04 12:00:00', 
+		"Tempo_estimado": '8 horas'
 	},
 	{ 
 		"ID": 2,
-		"Descrição": 'Importação de dados da Base Legada', 
-		"Data Máxima de conclusão": '2021-02-04 12:00:00', 
-		"Tempo estimado": '4 horas'
+		"Descricao": 'Importação de dados da Base Legada', 
+		"Data_Maxima_de_conclusao": '2021-02-04 12:00:00', 
+		"Tempo_estimado": '4 horas'
 	},
 	{ 
 		"ID": 3,
-		"Descrição": 'Importação de dados', 
-		"Data Máxima de conclusão": '2021-02-02 12:00:00', 
-		"Tempo estimado": '6 horas'
+		"Descricao": 'Importação de dados', 
+		"Data_Maxima_de_conclusao": '2021-02-02 12:00:00', 
+		"Tempo_estimado": '6 horas'
 	},
 { 
 		"ID": 4,
-		"Descrição": 'Desenvolver historia 745', 
-		"Data Máxima de conclusão": '2021-02-02 12:00:00', 
-		"Tempo estimado": '2 horas'
+		"Descricao": 'Desenvolver historia 745', 
+		"Data_Maxima_de_conclusao": '2021-02-02 12:00:00', 
+		"Tempo_estimado": '2 horas'
 	},
 	{ 
 		"ID": 5,
-		"Descrição": 'Gerar QRCode', 
-		"Data Máxima de conclusão": '2020-02-15 12:00:00', 
-		"Tempo estimado": '6 horas'
+		"Descricao": 'Gerar QRCode', 
+		"Data_Maxima_de_conclusao": '2020-02-15 12:00:00', 
+		"Tempo_estimado": '6 horas'
 	},
 	{
 		"ID": 6,
-		"Descrição": 'Importação de dados de integração', 
-		"Data Máxima de conclusão": '2020-02-15 12:00:00', 
-		"Tempo estimado": '8 horas'
+		"Descricao": 'Importação de dados de integração', 
+		"Data_Maxima_de_conclusao": '2020-02-15 12:00:00', 
+		"Tempo_estimado": '8 horas'
 	},
 ]
 
